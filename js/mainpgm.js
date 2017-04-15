035b20171b1c0b951d51bc705e3ba1192a8985f3
moodalagiri <manasa.moodalagiri@gmail.com>
2017-04-15 15:43:22 -0700

    first commit

---
+++ js/mainpgm.js
@@ -0,0 +1,26 @@
+/**
+ * Created by manasabharatesha on 9/27/2016.
+ */
+
+var MA=0;
+localStorage.setItem("MathAnswered",MA);
+var MC=0;
+localStorage.setItem("MathCorrect",MC);
+var EA = 0;
+localStorage.setItem("EngAnswered",EA);
+var EC = 0;
+localStorage.setItem("EngCorrect",EC);
+var AA = 0;
+localStorage.setItem("AudAnswered",AA);
+var AC = 0;
+localStorage.setItem("AudCorrect",AC);
+var QS  = 0;
+localStorage.setItem("quantScore",QS);
+var ES = 0;
+localStorage.setItem("engScore",ES);
+var AS = 0;
+localStorage.setItem("audScore",AS);
+
+
+
+
